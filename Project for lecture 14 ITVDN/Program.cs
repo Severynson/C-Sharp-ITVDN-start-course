c9614883b4ccd87016c696fc9bca39ea3006731c
Severyn <severynson.kurach@gmail.com>
2022-09-01 11:15:55 +0300

    Lecture 15 in progres...

---
+++ Project for lecture 14 ITVDN/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Project_for_lecture_14_ITVDN
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * 
+            string sentence = "User (says): ";
+
+            Console.WriteLine(sentence);
+
+            string readedSentenceFromUser = Console.ReadLine();
+
+            Console.WriteLine("Console: Now I'm displaying what you wrote: " + readedSentenceFromUser);
+
+            */
+
+            char letter = Convert.ToChar("A");
+            Console.WriteLine(letter);
+
+            string sentence = Convert.ToString('A');
+            Console.WriteLine(sentence);
+        }
+
+    }
+}
---
+++ Project for lecture 15 ITVDN/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Project_for_lecture_15_ITVDN
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*
+
+            string firstPart = "Hello", secondPart = "world";
+
+            string text = firstPart + " " + secondPart;
+
+            text += "!";
+
+            Console.WriteLine(text);
+
+            */
+
+            /*
+
+            Console.WriteLine("Write the augend:");
+            string stringAugend = Console.ReadLine();
+
+            Console.WriteLine("Write the addend:");
+            string stringAddend = Console.ReadLine();
+
+            int augend = Convert.ToInt32(stringAugend);
+            int addend = Convert.ToInt32(stringAddend);
+
+            int sum = augend + addend;
+
+            // string result = augend + " + " + addend + " = " + sum;
+
+            // Console.WriteLine(result);
+
+            string result = string.Format("Result: {0} + {1} = {2}", augend, addend, sum);
+
+            Console.WriteLine(result);
+
+            */
+
+            /*
+
+            Console.WriteLine("   format: {0}", 0.123456789_12345678);
+            // R formater used for more accurate result:
+            Console.WriteLine("R  format: {0:R}", 0.123456789_12345678);
+            
+            */
+
+            double interpolationExpression = 0.123456789_12345678;
+
+            Console.WriteLine("");
+
+            // Delay
+            Console.ReadKey();
+
+        }
+    }
+}
